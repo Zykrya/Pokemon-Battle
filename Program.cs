71ef9e56431516b7427c67cdc481af4a3c7cc3bb
Keylach <Keylachsalazar@gmail.com>
2023-03-03 21:00:57 -0600

    Project created

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int firstvalue = 10; // Explicitly typed
+var secondvalue = 5; // implicitly type
+secondvalue = 6;
+firstvalue = 11;
+
+Console.WriteLine(firstvalue); // 11
+
+int sum = firstvalue + secondvalue; 
+Console.WriteLine(sum); // 17
+
+int difference = firstvalue - secondvalue;
+Console.WriteLine(difference);
+
+int product = firstvalue * secondvalue;
+Console.WriteLine(product); //66
+
+int quotient = firstvalue / secondvalue;
+Console.WriteLine(quotient); //1
+
+int remainder = firstvalue % secondvalue;
+Console.WriteLine(remainder); // 5
+
+double thirdvalue = 10.0;
+double forthvalue = 3.0;
+
+double division = thirdvalue / forthvalue;
+Console.WriteLine(division); // 3.33
+
+double negation = -thirdvalue;
+Console.WriteLine(negation); // -10
+
+int x = 0;
+x++;
+x++;
+x++;
+Console.WriteLine(x); // 3
+x--;
+Console.WriteLine(x); // 2
+
+//1.4 -> 1 X
+//1.8 -> 2 X
+// 1.6 ~> 1 <-
+// double = 0.00 con decimales
+
+// 1 byte = 8 bits
+
+// 64MB * 1024 = 563,870,912
+
+// 8 32 64 128 256 768 1024
+
+// 1 bit = 0/1
